8a657e801506a92e031e2e632b32f9631c1800f7
js <jessetsnyder@gmail.com>
2024-02-04 11:34:09 -0700

    fix test copy

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders initial page', () => {
   render(<App />);
   const elem = screen.getByText(/nice/i);
   expect(elem).toBeInTheDocument();
