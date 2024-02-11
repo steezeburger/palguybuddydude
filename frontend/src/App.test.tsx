aaf472ea83ecb067d610ed0f302fc1c2c8c67306
js <jessetsnyder@gmail.com>
2024-02-10 23:39:50 -0700

    add biomejs

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders initial page', () => {
-  render(<App />);
-  const elem = screen.getByText(/nice/i);
-  expect(elem).toBeInTheDocument();
+test("renders initial page", () => {
+	render(<App />);
+	const elem = screen.getByText(/nice/i);
+	expect(elem).toBeInTheDocument();
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="has-text-centered is-size-1">
-      Nice
-    </div>
-  )
+	return <div className="has-text-centered is-size-1">Nice</div>;
 }
 
-export default App
+export default App;
